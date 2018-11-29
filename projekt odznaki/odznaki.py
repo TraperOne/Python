80709def28e05ac10210c290048ea13585ba8380
emil <emil.m@o2.pl>
2018-11-29 18:40:30 +0100

    dodanie kolumny łańcuchy

--- projekt odznaki/odznaki.py
+++ projekt odznaki/odznaki.py
@@ -100,15 +100,16 @@ class Odznaki:
 
     def badges(self):
         self.c.execute(
-            "SELECT nazwa_pasma, COUNT(DISTINCT nazwa_szczytu) FROM osiagniecia "
+            "SELECT nazwa, nazwa_pasma, COUNT(DISTINCT nazwa_szczytu) FROM osiagniecia "
             "JOIN uzytkownicy ON (osiagniecia.uzytkownicy_id_uzytkownicy = uzytkownicy.id_uzytkownicy) "
             "JOIN pasma_szczyty ON (pasma_szczyty.id_pasma_szczyty = osiagniecia.pasma_szczyty_id_pasma_szczyty) "
             "JOIN pasma_gorskie ON (pasma_gorskie.id_pasma_gorskie = pasma_szczyty.pasma_gorskie_id_pasma_gorskie) "
-            "WHERE uzytkownicy.login = %s GROUP BY nazwa_pasma", self.login)
+            "JOIN lancuchy_gorskie ON (lancuchy_gorskie.id_lancuchy_gorskie = pasma_gorskie.lancuchy_gorskie_id_lancuchy_gorskie) "
+            "WHERE uzytkownicy.login = %s GROUP BY pasma_gorskie_id_pasma_gorskie", self.login)
         Result = self.c.fetchall()
-        print("%20s|%20s" % ("nazwa pasma", "liczba szczytów"))
+        print("%20s|%20s|%20s" % ("łańcuchy górskie", "nazwa pasma", "liczba szczytów"))
         for row in Result:
-            print("%20s|%20s" % (row[0], row[1]))
+            print("%20s|%20s|%20s" % (row[0], row[1], row[2]))
 
     def delete(self):
         self.achievement()
